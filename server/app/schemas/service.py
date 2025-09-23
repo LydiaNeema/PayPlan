e9ab00566d551644dcb60ad1cf88aba8b15f8922
Lydia Neema <lydianeema25@gmail.com>
2025-09-23 20:41:50 +0300

    Add Service schema and update other schemas, ignore app.db

--- server/app/schemas/service.py
+++ server/app/schemas/service.py
@@ -1,6 +1,6 @@
 from marshmallow import Schema, fields
 
-class HouseholdSchema(Schema):
+class ServiceSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
-    created_at = fields.DateTime(dump_only=True)
+    description = fields.Str()
