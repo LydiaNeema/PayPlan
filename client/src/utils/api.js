3360bd01956de0b2f827783ffdf3a5f1cae81f54
Lydia Neema <lydianeema25@gmail.com>
2025-09-27 06:01:31 +0300

    updated render url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://payplan.onrender.com";
 
 export async function request(
   path,
